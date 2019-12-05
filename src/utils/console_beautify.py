68de98d9a6661bea9a8b76b03ed15ccc42d01c95
JacobCN <coder211@163.com>
2019-12-05 11:08:21 +0800

    修改工具包：美化控制台输出

--- src/utils/console_beautify.py
+++ src/utils/console_beautify.py
@@ -7,8 +7,10 @@
 from functools import wraps
 
 # 全局变量
-__cnt_simple = 0  # 全局计数器
-__sep_clr_font = '\033[95m'  # 分隔行字体颜色，紫红色
+__counter_for_segment = 0  # segment 全局计数器
+__counter_for_logger = 0  # logger 全局计数器
+__sep_clr_purple = '\033[95m'  # 分隔行字体颜色，紫红色
+__sep_clr_red = '\033[91m'  # 分割行字体颜色，红色
 __sep_clr_end = '\033[0m'  # 字体颜色设置结束符
 
 
@@ -19,16 +21,19 @@ def log_simple(func):
     格式：#<程序执行总次数> - [<执行函数的名称>]-<该函数执行次数> <输出30个等号（=）>
     """
 
-    __cnt_func = 0
+    __counter_func = 0  # 局部计数器
+
 
     @wraps(func)  # make original '__name__' is still original name
     def wrapper(*args, **kwargs):
-        global __cnt_simple #函数内部修改全局变量
-        __cnt_simple += 1
-        nonlocal __cnt_func #函数内部修改父级作用域的局部变量
-        __cnt_func += 1
-        line = __sep_clr_font + '#' + str(__cnt_simple) \
-               + ' - [' + func.__name__ + ']-' + str(__cnt_func) \
+        global __counter_for_logger  # 函数内部修改全局变量
+        nonlocal __counter_func  # 函数内部修改父级作用域的局部变量
+        if __counter_for_logger == 0:  # 如果全局计数器重置，则局部计数器自动重置
+            __counter_func = 0
+        __counter_func += 1
+        __counter_for_logger += 1
+        line = __sep_clr_purple + '#' + str(__counter_for_logger) \
+               + ' - [' + func.__name__ + ']-' + str(__counter_func) \
                + ' ' + ('=' * 30) + __sep_clr_end
         print(line)
         r = func(*args, **kwargs)
@@ -50,16 +55,18 @@ def log_advanced(sep: str = '=', n: int = 30):
     """
 
     def decorator(func):
-        __cnt_func = 0
+        __counter_func = 0  # 局部计数器
 
         @wraps(func)
         def wrapper(*args, **kwargs):
-            global __cnt_simple
-            __cnt_simple += 1
-            nonlocal __cnt_func
-            __cnt_func += 1
-            line = __sep_clr_font + '#' + str(__cnt_simple) \
-                   + ' - [' + func.__name__ + ']-' + str(__cnt_func) \
+            global __counter_for_logger
+            nonlocal __counter_func
+            if __counter_for_logger == 0:  # 如果全局计数器重置，则局部计数器自动重置
+                __counter_func = 0
+            __counter_for_logger += 1
+            __counter_func += 1
+            line = __sep_clr_purple + '#' + str(__counter_for_logger) \
+                   + ' - [' + func.__name__ + ']-' + str(__counter_func) \
                    + ' ' + (sep * n) + __sep_clr_end
             print(line)
             r = func(*args, **kwargs)
@@ -69,3 +76,26 @@ def log_advanced(sep: str = '=', n: int = 30):
         return wrapper
 
     return decorator
+
+
+# 正常函数，不是装饰器
+def reset_segment(msg: str = None, sep: str = '=', sep_cnt: int = 60) -> None:
+    """
+    重置log计数器, 并打印
+
+    打印格式：[##]<序号> [提示信息] < `sep_cnt` 个符号 `sep` >
+
+    :param msg: 提示信息
+    :param sep: 分隔符号
+    :param sep_cnt: 分隔符号数量
+    :return: 无
+    """
+    global __counter_for_logger
+    __counter_for_logger = 0
+    global __counter_for_segment
+    __counter_for_segment += 1
+    line = __sep_clr_red + '##' + str(__counter_for_segment)
+    if msg is not None:
+        line += ' [' + msg + ']'
+    line += ' ' + sep * sep_cnt
+    print(line + __sep_clr_end)
